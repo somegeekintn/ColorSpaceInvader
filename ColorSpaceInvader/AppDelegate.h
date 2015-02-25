de1dffbe1a204efaf456001069e0514660a11063
Casey Fleser <cfleser@gmail.com>
2015-02-25 15:14:53 -0600

    Initial Commit

---
+++ ColorSpaceInvader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorSpaceInvader
+//
+//  Created by Casey Fleser on 2/25/15.
+//  Copyright (c) 2015 Quiet Spark. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
